7112ca586f8220317caacd9a9ec2abf5d4b0c9b4
Quader <quaderkhan@gmail.com>
2025-11-16 18:37:09 +0530

    Updated Readme, package json and configs

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# MyReusableComponents
-React usable components
+# QuaderLibrary
+
+A modern, reusable React component library built by **Qadir**, featuring common UI elements, hooks, styles, and utility components.  
+Designed to be lightweight, fast, and easy to use across multiple React projects.
+
+---
+
+## 🚀 Installation
+
+Install the package from npm:
+
+```bash
+npm install quaderlibrary
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm', 'cjs'],
+    splitting: false,   // <- disables chunk files
     dts: true,
     sourcemap: true,
     clean: true,
     external: ['react', 'react-dom'],
+    loader: {
+        '.css': 'copy',   // 👈 FIX: direct css passthrough
+    },
     ignoreWatch: ['**/*.d.ts'], // ✅ safely ignore declarations
 });
